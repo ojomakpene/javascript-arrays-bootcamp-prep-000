dc6c013a7d9bdea2c5241f2eb1ee22aabd563bab
Sani Abubakar <egbaojo@gmail.com>
2017-08-26 13:55:04 +0000

    automatically backed up by learn

--- test/arrays-test.js
+++ test/arrays-test.js
@@ -45,4 +45,15 @@ it('alters `array`', () => {
  expect(array).to.eql(array)
   })
  })
+ describe('destructivelyAddElementToEndOfArray(array, element)', () => {
+ it('adds an `element` to the end of an `array`', () => {
+ expect(destructivelyAddElementToEndOfArray([1], 'foo')).to.eql([1, 'foo'])
+ })
+it('alters `array`', () => {
+const array = [1]
+destructivelyAddElementToEndOfArray(array, 'foo') 
+expect(array).to.eql([1, 'foo'])
+})
+ })
+  
 })

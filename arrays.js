8c40f00e763c1cbb2ad512c078adcad5fd61ce30
Sani Abubakar <egbaojo@gmail.com>
2017-08-26 09:15:21 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,7 @@
+var array = [1]
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+console.log(chocolateBars)
+function addElementToBeginningOfArray(array, element) {
+  ['foo', ...array]
+  return array
+}

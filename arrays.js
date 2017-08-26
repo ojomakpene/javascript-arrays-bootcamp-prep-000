c0cc612c1024f111d7249f8a2d975881e79be360
Sani Abubakar <egbaojo@gmail.com>
2017-08-26 13:18:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,41 +2,6 @@ var array = [1]
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 console.log(chocolateBars)
 function addElementToBeginningOfArray(array, element) {
-  ['foo', ...array]
-  return array
-}
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift["foo"]
-  return array
-}
-function addElementToEndOfArray(array, element) {
-  [...array, 1]
-  return array
-}
-function destructivelyAddElementToEndOfArray(array, element) {
-  array.push['foo']
-  return array
-}
-function accessElementInArray(array, index) {
-var array = [1, 2, 3]
- console.log(accessElementInArray[2])
-}
-function destructivelyRemoveElementFromBeginningOfArray(array) {
-  var array = [1, 2, 3]
-  array.shift()
-  return array
-}
-function removeElementFromBeginningOfArray(array) {
- var array = [1, 2, 3]
- return  array.slice(1)
-}
-function destructivelyRemoveElementFromEndOfArray(array) {
-  var array = [1, 2, 3]
-  array.pop()
-  return array
-}
-function removeElementFromEndOfArray(array) {
-  var array = [1, 2, 3]
-  array.slice(0, array.length - 1)
+array = ['foo', ...array]
   return array
 }

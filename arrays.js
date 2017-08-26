48ed410c4a996be22b2d3b705f3d42f925ba2d17
Sani Abubakar <egbaojo@gmail.com>
2017-08-26 10:28:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,26 +17,4 @@ function destructivelyAddElementToEndOfArray(array, element) {
   array.push['foo']
   return array
 }
-function accessElementInArray(array, index) {
-  var array = [1, 2, 3]
- console.log(accessElementInArray[2])
-}
-function destructivelyRemoveElementFromBeginningOfArray(array) {
-  var array = [1, 2, 3]
-  array.shift()
-  return array
-}
-function removeElementFromBeginningOfArray(array) {
-  var array = [1, 2, 3]
- return  array.slice(1)
-}
-function destructivelyRemoveElementFromEndOfArray(array) {
-  var array = [1, 2, 3]
-  array.pop()
-  return array
-}
-function removeElementFromEndOfArray(array) {
-  var array = [1, 2, 3]
-  array.slice(0, array.length - 1)
-  return array
-}
+

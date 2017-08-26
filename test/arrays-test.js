d26244829da89ce35fcf177d54af4756000a8eb0
Sani Abubakar <egbaojo@gmail.com>
2017-08-26 13:17:09 +0000

    automatically backed up by learn

--- test/arrays-test.js
+++ test/arrays-test.js
@@ -15,4 +15,16 @@ describe('arrays', () => {
       expect(chocolateBars).to.eql['snickers', 'hundred grand', 'kitkat', 'skittles']
     })
   })
+  describe('addElementToBeginningOfArray(array, element)', () => {
+  it('adds an `element` to the beginning of an `array`', () => {
+    expect(addElementToBeginningOfArray([1], 'foo')).to.eql(['foo', 1])
+    })
+it('does not alter `array`', () => {
+   const array = [1]
+addElementToBeginningOfArray(array, 'foo')
+expect(array).to.eql([1])
+  })
+
+ })
+
 })

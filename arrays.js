a3f5837321a5ea275f37d70967c8f3b686b7bfba
Sani Abubakar <egbaojo@gmail.com>
2017-08-26 12:25:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,3 +35,8 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
   return array
 }
+function removeElementFromEndOfArray(array) {
+  var array = [1, 2, 3]
+  array.slice(0, array.length - 1)
+  return array
+}

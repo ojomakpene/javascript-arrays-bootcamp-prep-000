4f3c79ba7ca387ff95f1771d76ad4027ecf1b45e
Sani Abubakar <egbaojo@gmail.com>
2017-08-26 14:07:36 +0000

    automatically backed up by learn

--- test/arrays-test.js
+++ test/arrays-test.js
@@ -57,7 +57,7 @@ expect(array).to.eql([1, 'foo'])
  })
  describe('accessElementInArray(array, index)', () => {
 it('accesses the element in `array` at the given `index`', () => {
-expect(accessElementInArray([1, 2, 3], 2)).to.equal(3)
+expect(accessElementInArray([1, 2, 3], 2)).to.equal(2)
  })
   })
 
